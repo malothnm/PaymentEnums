ea73144aa727febf9e6b3358e108bfb1f13cd332
Nithin Maloth <francis.nithin@yahoo.in>
2021-06-01 15:08:48 +0530

    Updates for responseFields

--- src/main/java/in/nmaloth/payments/constants/ids/ServiceID.java
+++ src/main/java/in/nmaloth/payments/constants/ids/ServiceID.java
@@ -8,10 +8,7 @@ public class ServiceID {
     public static final String CURRENCY_CONVERSION = "CURRENCY_CONVERSION";
     public static final String MESSAGE_DISTRIBUTOR = "MESSAGE_DISTRIBUTOR";
     public static final String ADDRESS_VERIFIER = "ADDRESS_VERIFIER";
-    public static final String CVC_VALIDATION_SERVICE = "CVC";
-    public static final String CVC2_VALIDATION_SERVICE = "CVC2";
-    public static final String CRYPTOGRAM_VALIDATION_SERVICE = "CRYPTOGRAM";
-    public static final String CAVV_VALIDATION_SERVICE = "CAVV_VALIDATION";
+    public static final String CRYPTO_SERVICE = "CRYPTO_SERVICE";
     public static final String SCRIPT_GENERATION_SERVICE = "SCRIPT";
     public static final String AGGREGATOR = "AGGREGATOR";
 
